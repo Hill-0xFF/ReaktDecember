04e3002f33cb505e7061dea340b719968e05d36c
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-12-22 22:30:27 -0800

    [fix]: Chap18 - Fixed the responsiveness when testing it. Added the root id to make grow and flex just like the body element.

--- chap17-routerhookslinks/src/components/About.tsx
+++ chap17-routerhookslinks/src/components/About.tsx
@@ -1,3 +1,12 @@
 export default function About() {
-  return <main>About</main>;
+  return (
+    <main className="about">
+      <h2 style={{ backgroundColor: '#451abf' }}>Welcome,</h2>
+      <p>
+        I&apos;m currently a Fullstack Software Developer student. Created this
+        blog as a project, a real blog (duh) and to test and implent things
+        I&apos;m learning at this moment.
+      </p>
+    </main>
+  );
 }
