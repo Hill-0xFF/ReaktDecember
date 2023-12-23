7cbbe2235655f1a00a85e4175875ba126fab1520
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-12-23 10:06:47 -0800

    [fix]: Chap20 - refactored cleanup function for useWindowSize hook.

--- chap20-customhooks/src/hooks/useWindowSize.ts
+++ chap20-customhooks/src/hooks/useWindowSize.ts
@@ -18,11 +18,18 @@ export default function useWindowSize() {
     window.addEventListener('resize', handleResize);
 
     // cleanup code
-    function cleanUp() {
-      console.log('Runs if a useEffect dependency changes');
+    // function cleanUp() {
+    //   console.log('Runs if a useEffect dependency changes');
+    //   window.removeEventListener('resize', handleResize);
+    // }
+    // return cleanUp;
+    /**
+     * Some code as above, just an anonymous function return at the end of
+     * a hook, which indicates a cleanup function/operation (to prevent memory leaks!)
+     */
+    return function () {
       window.removeEventListener('resize', handleResize);
-    }
-    return cleanUp;
+    };
   }, []);
   return windowSize;
 }
